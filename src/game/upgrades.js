fd973285e219765bc15e9220467d661d54feb23c
Andre <andrewbornam@gmail.com>
2026-09-01 04:26:00 -0300

    Fix / Finish

--- src/game/upgrades.js
+++ src/game/upgrades.js
@@ -1,9 +1,6 @@
 import { CARD_DEFS, BULLET_DAMAGE_STEP, BULLET_RADIUS_STEP, FIRE_RATE_MULTIPLIER_STEP, MAX_SLOW_PERCENT, SLOW_STEP } from "./constants.js";
 
-
-
 //player upgrades
-
 
 
 export function applyUpgrade(upgradeId, s){
@@ -11,44 +8,28 @@ export function applyUpgrade(upgradeId, s){
     if (upgradeId === "bigger_bullets") {
 
         s.upgrades.bulletRadiusBonus += BULLET_RADIUS_STEP;
-
     } else if (upgradeId === "faster_shooting") {
-
         s.upgrades.fireIntervalMultiplier *= FIRE_RATE_MULTIPLIER_STEP;
-
     } else if (upgradeId === "more_damage") {
-
         s.upgrades.bulletDamage += BULLET_DAMAGE_STEP;
-
     } else if (upgradeId === "slowing_field") {
-
         s.upgrades.slowPercent = Math.min(MAX_SLOW_PERCENT, s.upgrades.slowPercent + SLOW_STEP);
-
     }
 
 }
-
 
 
 export function shuffledCards(){
 
     const copy =[...CARD_DEFS];
 
-
-
     for (let i = copy.length - 1; i > 0; i--) {
 
         const j = Math.floor(Math.random() * (i + 1));
-
-
-
         const temp = copy[i];
 
         copy[i] = copy[j];
-
         copy[j] = temp;
-
-
 
     }
 
